6d4bea19ec86967547dc3b54e67f3730614cf9fc
macknilan <rodolfougaldeochoa@gmail.com>
2015-08-24 22:27:36 -0500

    Mod. para poner ruta adecuada imagenes 1.8

--- muebleria/settings/prod.py
+++ muebleria/settings/prod.py
@@ -2,7 +2,7 @@
 from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('MUEBLERIALLAVE')
+SECRET_KEY = os.environ.get('MUEBLERIALLAVE')
 
 DEBUG = False
 
